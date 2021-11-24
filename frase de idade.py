147de2460d67749f58893f95eeee1bbf614a00f5
Vinicius Santos <vinicius.3991@gmail.com>
2021-11-23 21:19:59 -0300

    primeiro passo no python do @drozinho

---
+++ frase de idade.py
@@ -0,0 +1,6 @@
+nome = input('Qual é seu nome?')
+dia = input('Digite apenas o DIA em que você nasceu:')
+mês = input('Digite apenas o MÊS em que você nasceu:')
+ano = input('Digite apenas o ANO em que você nasceu:')
+idade = input('Digite a sua idade atual:')
+print("Olá, meu nome é", nome+',', "nasci no dia", dia, "de", mês, "do ano de", ano+',', "portanto, atualmente tenho", idade, "anos.")
