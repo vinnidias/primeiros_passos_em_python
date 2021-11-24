2842654c900102c77e34d0e012882b20505d33a6
Vinicius Santos <vinicius.3991@gmail.com>
2021-11-23 22:22:11 -0300

    ensinando o pull para o @drozinho

--- nome e boas vindas.py
+++ nome e boas vindas.py
@@ -1,3 +1,4 @@
 mensagem = input('Qual é seu nome?') 
 boas_vindas = ('Seja Muito bem vindo ao Python')
 print("Olá, meu querido", mensagem+',', boas_vindas)
+print("finalizando aulinha de git com pull")
